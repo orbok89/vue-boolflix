09ac7797b2656a8bea632cb32e3a9090256b2861
BigFelk <fedferrara89@gmail.com>
2021-09-24 11:14:33 +0200

    header fatto

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <header>
+          <h1>boolflix</h1>
+          <input type="text" placeholder="cerca film">
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+header{
+    width: 100%;
+    height: 100px;
+    background-color: black;
+}
+h1{
+    color: red;
+}
+</style>
